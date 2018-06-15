05b148d97481db2e0ef6bfb90905d80573f09f9b
Jordi Nebot <jordi@lovetoknow.com>
2018-06-15 20:28:17 +0200

    Implement End-with and Words-with

---
+++ run.php
@@ -0,0 +1,49 @@
+<?php
+
+$inputFiles = [
+    __DIR__ . '/data/scrabble2.txt',
+    __DIR__ . '/data/scrabble3.txt',
+    __DIR__ . '/data/scrabble4.txt',
+    __DIR__ . '/data/scrabble5.txt',
+    __DIR__ . '/data/scrabble6.txt',
+    __DIR__ . '/data/scrabble7.txt',
+    __DIR__ . '/data/scrabble8.txt',
+    __DIR__ . '/data/scrabble9.txt',
+    __DIR__ . '/data/scrabble10.txt',
+    __DIR__ . '/data/scrabble11.txt',
+    __DIR__ . '/data/scrabble12.txt',
+];
+
+$output = [
+    'start-with' => [],
+    'end-with'   => [],
+    'words-with' => [],
+];
+foreach ($inputFiles as $filename) {
+    $ifh = new SplFileObject($filename);
+    while ($ifh->valid()) {
+        $line = trim($ifh->fgets());
+
+        if (empty($line))
+            continue;
+
+        $len = strlen($line);
+        $firstChar = $line[0];
+        $lastChar = $line[$len - 1];
+
+        $output['start-with'][$firstChar][$len][] = $line;
+        $output['end-with'][$lastChar][$len][] = $line;
+
+        foreach(str_split($line) as $c) {
+            $output['words-with'][$c][$len][] = $line;
+        }
+    }
+    $ifh = null;
+
+    // Store output
+    foreach ($output as $set => $data) {
+        $ofh = new SplFileObject("./output/$set.json", 'w');
+        $ofh->fwrite(json_encode($data));
+        $ofh = null;
+    }
+}
